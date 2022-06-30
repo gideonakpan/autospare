ef10bcce89823cd0c1f5ab582c0247523d943c51
gideonakpan <gideonakpan98@yahoo.com>
2022-06-30 20:45:54 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+
+		function navMenu() {
+			var x = document.getElementById('nav-menu')
+			if (x.style.display === 'block') {
+				x.style.display = 'none'
+			}else {
+				x.style.display = 'block'
+			}
+		}
+
+
+		function changeStyle(x) {
+		  x.classList.toggle("change");
+		}
+
+
+		
